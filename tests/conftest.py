98fab7838a9989435867128dbf71df821dc0866c
DARKZOUL5 <mihailv@inbox.lv>
2026-05-16 16:13:50 +0300

    refactor: remove old tests

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from tests.dummy_config import DummyConfig
-
-
-@pytest.fixture
-def dummy_config():
-    """Return a fresh DummyConfig instance for tests to customize."""
-    return DummyConfig()
